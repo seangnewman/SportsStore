be0daf189c5f8dffe5b7d53860fb6a95e897b64b
Sean Newman <seangnewman@msn.com>
2018-05-03 07:26:42 -0700

    Completed updates to URL schemes

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -50,8 +50,17 @@ namespace SportsStore
             app.UseMvc(routes => {
                 //Setup the middleware to inspect routing. 
                 //Send request to list action or the product controller
-                routes.MapRoute(name: "pagination", template: "Products/Page{productPage}", defaults: new { Controller="Product", action="List"});
-                routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
+                routes.MapRoute(name: null, template: "{category}/Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List" });
+                routes.MapRoute(name: null, template: "Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(name: null, template: "{category}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(name: null, template : "",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+                //routes.MapRoute(name: "pagination", template: "Products/Page{productPage}", defaults: new { Controller="Product", action="List"});
+                //routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
             });
 
             SeedData.EnsurePopulated(app);
